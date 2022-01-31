cca0246170eb71bd0823f8c55e41663979444aeb
Jeferson Macedo <jefersonmacedowgf@gmail.com>
2022-01-31 12:09:14 -0300

    Videos no Profile 2

--- src/components/Photos/Photos.jsx
+++ src/components/Photos/Photos.jsx
@@ -1,5 +1,4 @@
-import { useEffect } from "react";
-import { useState } from "react/cjs/react.development";
+import { useEffect, useState } from "react";
 import api from "../../services/api";
 import './photos.css'
 
