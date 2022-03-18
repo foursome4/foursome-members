2aa20ffb137660c811837ec711c9e437778fc914
Jeferson Macedo <jefersonmacedowgf@gmail.com>
2022-03-18 08:54:37 -0300

    Invites

--- src/components/ComponentsInvites/InviteWhatsapp/InviteWhatsapp.jsx
+++ src/components/ComponentsInvites/InviteWhatsapp/InviteWhatsapp.jsx
@@ -20,59 +20,28 @@ function InviteWhatsapp() {
 
     function createInviteWhatsapp(e) {
         e.preventDefault();
+        if(phone.length !== 11) {
+            console.log(phone)
+            toast.error("Telefone incorreto. siga a instrução DDD + Número")
+            return
+        }
         toast.info("Enviando convite. Aguarde...")
-
-        const remove1Paranteses = phone.replace('(', '')
-        const remove2Paranteses = remove1Paranteses.replace(')', '')
-        const removeSpace = remove2Paranteses.replace(' ', '')
-        const removeTrace = removeSpace.replace('-', '')
-        const newPhone = removeTrace;
-
-        console.log(newPhone)
-        const inviteCode = uuidv4()
-       
+        const inviteCode = uuidv4();     
         const code = inviteCode.substring(0, 4)
 
-        console.log(`Code: ${code}, Nome: ${name}, Email: ${email}, Telefone: ${newPhone},
-        isAccount: ${user.id}, username: ${user.username}, nickname: ${userInformation.nickname}, avatar: ${userInformation.avatar}`);
-
-
-       CreateInviteNewUsew({code, name, email, phone:newPhone, username: user.username, idAccount: user.id, patron: user.id, patronNickname:userInformation.nickname })
+       CreateInviteNewUsew({code, name, email, phone, username: user.username, idAccount: user.id, patron: user.id, patronNickname:userInformation.nickname })
 
         setEmail("")
         setPhone("")
         setName("")
     }
-
-    function mascaraFone(event) {
-        var valor = document.getElementById("telefone").attributes[0].ownerElement['value'];
-        var retorno = valor.replace(/\D/g,"");
-        retorno = retorno.replace(/^0/,"");
-        if (retorno.length > 10) {
-          retorno = retorno.replace(/^(\d\d)(\d{5})(\d{4}).*/,"($1) $2-$3");
-        } else if (retorno.length > 5) {
-          if (retorno.length === 6 && event.code === "Backspace") { 
-            // necessário pois senão o "-" fica sempre voltando ao dar backspace
-            return; 
-          } 
-          retorno = retorno.replace(/^(\d\d)(\d{4})(\d{0,4}).*/,"($1) $2-$3");
-        } else if (retorno.length > 2) {
-          retorno = retorno.replace(/^(\d\d)(\d{0,5})/,"($1) $2");
-        } else {
-          if (retorno.length !== 0) {
-            retorno = retorno.replace(/^(\d*)/,"($1");
-          }
-        }
-        document.getElementById("telefone").attributes[0].ownerElement['value'] = retorno;
-      }
-
     return (
         <>
         <form action="">
                 <span>Convite por whatsapp</span>
                 <input type="text" value={name} onChange={(e) => setName(e.target.value)} placeholder="Nome"/>
                 <input type="text" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Email"/>
-                <input type="text" id="telefone" onKeyUp={mascaraFone} value={phone} onChange={(e) => setPhone(e.target.value)} placeholder="(XX)XXXXX-XXXX"/>
+                <input type="text" id="telefone"  value={phone} onChange={(e) => setPhone(e.target.value)} placeholder="Ex.: 21999888899"/>
 
                 <button onClick={createInviteWhatsapp}> Enviar Convite</button>
 
