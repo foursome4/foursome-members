9f440aefb3768393b1016507631b10dc3cabe504
Jeferson Macedo <jefersonmacedowgf@gmail.com>
2022-04-06 13:04:45 -0300

    Mensagens

--- src/components/ButtonsTopBar/MyMessages/UserConversation/UserConversation.jsx
+++ src/components/ButtonsTopBar/MyMessages/UserConversation/UserConversation.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState, useMemo } from "react";
 import { Link } from "react-router-dom";
+import { useFetch } from "../../../../hooks/useFetch";
 import api from "../../../../services/api";
 import './userConversation.css'
 
@@ -9,7 +10,6 @@ function UserConversation({idAccount, room}) {
 
     const [nickname, setNickname] = useState('')
     const [avatar, setAvatar] = useState('')
-    const [messages, setMessages] = useState([])
 
     useEffect(() => {
         async function loadInformations() {
@@ -21,40 +21,29 @@ function UserConversation({idAccount, room}) {
                 console.log("Erro aos buscar informações")
             })
         }
-
-        async function loadMesages() {
-            api.get(`/messages/${room}`).then((result) => {
-                setMessages(result.data);
-            }).catch((error) => {
-                console.log(error)
-                console.log("Erro aos buscar informações")
-            })
-        }
-
         loadInformations()
-        loadMesages()
     }, [idAccount, room ]);
 
  
 
-     const myMessages = useMemo(() => {
-        messages.filter((message) => (message.idAccount === user.id));
-    }, [messages, user.id]) ;
+    const {data} = useFetch(`/messages/${room}`);
 
-    const friendMessage = useMemo(() => {
-    messages.filter((message) => (message.idAccount !== user.id));
-    }, [messages, user.id]) ;
+    let myMessages = [];
+    let friendMessage = [];
+    let newMessages = [];
 
-    const newMessages = useMemo(() => {
-            if(myMessages !== undefined && friendMessage !== undefined ) {
-        friendMessage.filter((messages) => (new Date(messages.created_at) > new Date(myMessages[0].created_at)));
+    if(data) {
+
+        myMessages = data?.filter((message) => (message.idAccount === user.id));
+
+        friendMessage =  data?.filter((message) => (message.idAccount !== user.id));
+
+        newMessages = friendMessage.filter((messages) => (myMessages !== undefined && friendMessage !== undefined ? new Date(messages.created_at) > new Date(myMessages[0].created_at) : ""))
     }
-    }, [friendMessage,myMessages ]) ;
- 
 
 
     return (
-        messages.length === 0 ? "" :       
+        data?.length === 0 ? "" :       
        <div className="item">
            <div className="image">
            <Link to={`/chat/${room}/${idAccount}`}>
@@ -64,9 +53,9 @@ function UserConversation({idAccount, room}) {
            <Link to={`/chat/${room}/${idAccount}`}>
            <h4>Eu & {nickname}</h4>
            </Link>
-           {newMessages === undefined ? "" :
+           {newMessages.length === 0 ? "" :
            <div className="counter">
-               {newMessages.length}
+               {newMessages.length }
             </div>
 }
        </div>
