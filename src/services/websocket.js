6d3a6e448b0b3ddfdedc4c8dab2e5f7b812ac250
Jeferson Macedo <jefersonmacedowgf@gmail.com>
2022-06-06 15:37:33 -0300

    Websocket

--- src/services/websocket.js
+++ src/services/websocket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const socket = io('https://foursome4-api.herokuapp.com'');
+const socket = io('https://foursome4-api.herokuapp.com');
 //const socket = io('https://api-foursome.herokuapp.com/');
 //const socket = io('http://localhost:3333/');
 
