b25f6a014db76b12b2102690fb5ceb1d12dd6f2c
Jeferson Macedo <jefersonmacedowgf@gmail.com>
2022-08-02 13:44:56 -0300

    Retirando Api Google onde não se usa mais

--- src/contexts/Auth.jsx
+++ src/contexts/Auth.jsx
@@ -1,8 +1,6 @@
 import {createContext, useState} from 'react';
-import { useNavigate } from 'react-router-dom';
 import {toast} from 'react-toastify';
 import api from '../services/api';
-import apiGoogleReverse from '../services/apiGoogleReverse';
 import { socket } from '../services/websocket';
 import {v4 as uuidv4} from 'uuid';
 
