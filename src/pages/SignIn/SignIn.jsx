2f321a7afc576fbfa3f8fc88b432e12ee685fd7c
Jeferson Macedo <jefersonmacedowgf@gmail.com>
2022-08-12 12:43:08 -0300

    Atualização

--- src/pages/SignIn/SignIn.jsx
+++ src/pages/SignIn/SignIn.jsx
@@ -64,7 +64,7 @@ function SignIn() {
         <img src={logoImg} alt="Logo Foursome" />
         </div>
         <div className="form">
-          <input type="text" placeholder="E-mail ou Nome de usuário" value={login} onChange={(e) => setLogin(e.target.value)}/>
+          {/* <input type="text" placeholder="E-mail ou Nome de usuário" value={login} onChange={(e) => setLogin(e.target.value)}/>
           {never1 === true ? <h6>Favor remover os espaços vazios no campo login</h6> : ""}
           <div className="inputPassword">
           <input type={passwordView === false ? "password" : "text" } placeholder="Senha" value={password} onChange={(e) => setPassword(e.target.value)}/>
@@ -85,14 +85,19 @@ function SignIn() {
           <div className="links">
           <a href="/recuperation"><p> ESQUECI MINHA SENHA</p></a>
             <a href="/recuperationuser"><p> ESQUECI MEU USUÁRIO</p></a>
-          </div>
+          </div> */}
 
-          <div className="whatsapp" onClick={handleSignUp}>
+          {/* <div className="whatsapp" onClick={handleSignUp}>
              <h4><b>Não tem conta? </b> Clique aqui e cadastre-se</h4>
           </div>
           <div className="whatsapp2" onClick={handleInviteWhatsapp}>
             <h4> Entre em contato <IoLogoWhatsapp /></h4>
+          </div> */}
+
+          <div className="whatsapp" onClick={handleSignUp}>
+             <h4><b>Estamos em atualização. Retornaremos em breve! </b></h4>
           </div>
+
           {/* <div className="register">
           <h4><b>Não tem conta? </b> <a href="/signup">Clique aqui e cadastre-se</a></h4>
           </div> */}
